8a658cae0ec92a7ad4c3f558f7e4e641af2ea3a8
TonyStark992 <maxime.castillo@gmail.com>
2020-04-21 22:42:38 +0200

    Version 3.1 presque finie

--- app_3.rb
+++ app_3.rb
@@ -4,6 +4,23 @@ Bundler.require
 require_relative 'lib/game.rb'
 require_relative 'lib/player.rb'
 
-my_game = Game.new("Wolverine")
+puts "      -------------------------------------------------"
+puts "      |Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |"
+puts "      |Le but du jeu est d'être le dernier survivant !|"
+puts "      -------------------------------------------------"
 
-binding.pry
+puts "Pour commencer, indiquez votre pseudo :"
+print "> "
+my_game = Game.new(gets.chomp)
+
+while my_game.is_still_ongoing?
+  my_game.menu
+  print "> "
+  my_game.menu_choice(gets.chomp)
+  puts "--------------------------"
+  sleep 3
+  my_game.ennemies_attack
+  sleep 3
+end
+sleep 2
+my_game.end
--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,10 @@ class Game
   def initialize (name)
     @user = HumanPlayer.new(name)
     @enemies = []
-    @enemies.push(Player.new("player#{i}"))
+    @enemies.push(player1 = Player.new("Magneto"))
+    @enemies.push(player2 = Player.new("Dents-de-sabre"))
+    @enemies.push(player3 = Player.new("Mystique"))
+    @enemies.push(player4 = Player.new("Pyro"))
   end
 
   def kill_player (player_to_kill)
@@ -21,11 +24,67 @@ class Game
 
   def show_players
     puts @user.show_state
+    puts
     @enemies.each do |player|
       puts player.show_state
     end
   end
 
+  def menu
+    puts "--------------------------------------------------"
+    show_players
+    puts
+    puts "Quelle action veux-tu effectuer ?"
+    puts "a - Chercher une meilleure arme"
+    puts "s - Chercher à se soigner"
+    puts
+    puts "Attaquer un joueur en vue :"
+    puts "0 - #{@enemies[0].name}"
+    puts "1 - #{@enemies[1].name}"
+    puts "2 - #{@enemies[2].name}"
+    puts "3 - #{@enemies[3].name}"
+    puts
+  end
 
+  def menu_choice (input)
+    @enemies.each do |player|
+      kill_player(player.name) if player.life_points <= 0
+    end
+    case input
+    when "a"
+      @user.search_weapon
+    when "s"
+      @user.search_health_pack
+    when "0"
+      @user.attacks(@enemies[0])
+    when "1"
+      @user.attacks(@enemies[1])
+    when "2"
+      @user.attacks(@enemies[2])
+    when "3"
+      @user.attacks(@enemies[3])
+    end
+  end
+
+  def ennemies_attack
+    puts "Les autres joueurs t'attaquent !"
+    puts
+    @enemies.each do |player|
+      if player.life_points > 0
+        player.attacks(@user)
+      end
+    end
+  end
+
+  def end
+    if !is_still_ongoing?
+      puts "La partie est finie"
+      if @user.life_points > 0
+        puts "BRAVO ! TU AS GAGNE !"
+      else
+        puts "Loser ! Tu as perdu !"
+      end
+    end
+  end
 
 end
